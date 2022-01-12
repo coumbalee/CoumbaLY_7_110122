90e49b04e3e9541d871c686fe249f0e7134df962
coumbalee <coumba.ly@yahoo.com>
2022-01-13 00:55:09 +0100

    generateSearchBar

--- index.js
+++ index.js
@@ -12,3 +12,18 @@ function generateHeader() {
   headerLink.prepend(headerLogo);
 }
 generateHeader();
+function generateSearchBar() {
+  const searchSection = document.querySelector(".section-search");
+  console.log(searchSection);
+  const searchBar = document.createElement("div");
+  searchBar.classList.add("section-search__bar");
+  const inputSearch = document.createElement("input");
+  inputSearch.classList.add("section-search__input");
+  inputSearch.placeholder = `Rechercher un ingrédient, appareil, ustensil ou une recette`;
+  const searchBarButton = document.createElement("button");
+  searchBarButton.classList.add("section-search__button");
+  searchBarButton.innerHTML += `<i class="fas fa-search"></i>`;
+  searchSection.prepend(searchBar);
+  searchBar.append(inputSearch, searchBarButton);
+}
+generateSearchBar();
