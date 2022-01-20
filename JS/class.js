2edc40d00c32ddce13c424490417c333132e18b4
coumbalee <coumba.ly@yahoo.com>
2022-01-20 15:37:19 +0100

    Ajout id à l' ul

--- JS/class.js
+++ JS/class.js
@@ -7,8 +7,8 @@ export default class Select {
     return `
     <div class="section-filters__custom-select">
         <span class="section-filters__arrow  filters__arrow--${this.color}"> <i class="fas fa-angle-up"></i></span>
-        <input class="section-filters__select filters__select--${this.color}" type="button" data-color="${this.color}" value="${this.value}">
-        <ul class="section-filters__list section-filters__list--${this.color}"></ul>
+        <input  class="section-filters__select filters__select--${this.color}" type="button" data-color="${this.color}" value="${this.value}">
+        <ul id = "${this.color}" class="section-filters__list section-filters__list--${this.color}"></ul>
     </div>
     `;
   }
