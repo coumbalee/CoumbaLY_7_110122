5cb95f0f35ae657e5d390fb951756228dea31f48
coumbalee <coumba.ly@yahoo.com>
2022-01-20 19:26:57 +0100

    Suppression du header (crée en html)

--- JS/index.js
+++ JS/index.js
@@ -7,23 +7,9 @@ import {
 } from "./dataRecipes.js";
 import { displayDropdowns } from "./dropdowns.js";
 import { generateSearchBar } from "./searchBar.js";
-recipes;
 
-// HEADER
-function generateHeader() {
-  const header = document.querySelector("header");
-  const headerLink = document.createElement("a");
-  const headerLogo = document.createElement("img");
-  headerLogo.src = "/IMAGES/logo.png";
-  headerLogo.setAttribute("alt", "logo les petits plats");
-  headerLogo.classList.add("header__logo");
-  header.classList.add("header");
-  header.prepend(headerLink);
-  headerLink.classList.add("header__link");
-  headerLink.prepend(headerLogo);
-}
 async function displayPage() {
-  generateHeader();
+  // generateHeader();
   generateSearchBar();
   displayDropdowns();
 }
