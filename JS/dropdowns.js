39c9b5b9ee97307464cad271b9651644502a8681
coumbalee <coumba.ly@yahoo.com>
2022-01-20 20:36:07 +0100

    modification des imports de SelectClass

--- JS/dropdowns.js
+++ JS/dropdowns.js
@@ -1,49 +1,53 @@
 // export function generateDropdown()
-import Select, {
+import Select from "./SelectClass.js";
+console.log(Select);
+import {
   generateApplianceSelect,
   generateUstensilsSelect,
   generateIngrédientsSelect,
-} from "./class.js";
-console.log(Select);
+} from "./SelectClass.js";
 import {
   getAllAppliancesFromRecipes,
   getAllUstensilsFromRecipes,
   getAllIngredientsFromRecipes,
 } from "./dataRecipes.js";
+import { recipes } from "./recipes.js";
 
 export function displayDropdowns() {
+  const ingrédients = generateIngrédientsSelect();
   const appliance = generateApplianceSelect();
   const ustensils = generateUstensilsSelect();
-  const ingrédients = generateIngrédientsSelect();
-  addListenersToDropDowns(appliance, ustensils, ingrédients);
+
+  addListenersToDropDowns([ingrédients, appliance, ustensils]);
 }
 
-function addListenersToDropDowns(recipes, value) {
+function addListenersToDropDowns(values, index) {
   let elts = Array.from(document.querySelectorAll(".section-filters__select"));
   elts.forEach((elt) =>
     elt.addEventListener("click", (e) => {
       e.preventDefault();
       const sortOptions = sortOptionsByButtonValue(
         recipes,
-        value
-        // e.target.dataset.filter
+        values[index],
+        index
       );
-      // console.log(e.target.dataset.filter);
+      console.log(sortOptions);
+
       displayOptions(sortOptions);
     })
   );
 }
 
 // récupérer les options selon la valeur du bouton
-function sortOptionsByButtonValue(recipes, value, filter) {
-  switch (value) {
-    case "Ingrédients":
-      return getAllIngredientsFromRecipes(recipes, filter);
-    case "Appareils":
-      return getAllAppliancesFromRecipes(recipes, filter);
+function sortOptionsByButtonValue(index) {
+  switch (index) {
+    case 0:
+      return getAllIngredientsFromRecipes();
+    case 1:
+      return getAllAppliancesFromRecipes();
 
-    case "Ustensiles":
-      return getAllUstensilsFromRecipes(recipes, filter);
+    case 2:
+      return getAllUstensilsFromRecipes();
 
     default:
       break;
@@ -51,32 +55,29 @@ function sortOptionsByButtonValue(recipes, value, filter) {
 }
 // tri pour les placeholders des input
 
-function displayPlaceholderByButtonValue(value) {
-  const input = document.querySelectorAll(".section-filters__select");
-  switch (value) {
-    case "Ingrédients":
-      input.innerHtml = ` Recherche un ingrédient`;
+// function displayPlaceholderByButtonValue(value) {
+//   const input = document.querySelectorAll(".section-filters__select");
+//   switch (value) {
+//     case "Ingrédients":
+//       input.innerHtml = ` Recherche un ingrédient`;
 
-    case "Appareils":
-      input.innerHtml = ` Recherche un appareil`;
+//     case "Appareils":
+//       input.innerHtml = ` Recherche un appareil`;
 
-    case "Ustensiles":
-      input.innerHtml = ` Recherche un ustensil`;
+//     case "Ustensiles":
+//       input.innerHtml = ` Recherche un ustensil`;
 
-    default:
-      break;
-  }
-}
+//     default:
+//       break;
+//   }
+// }
 
-// afficher les options avec function displayOptions et y appeler la fonction displayPlaceholderByButtonValue
-function displayOptions() {
-  displayApllianceOptions();
+function displayOptions(options) {
+  displayApllianceOptions(options);
   // displayIngrédientsOptions();
   // displayUstensilsOptions();
 }
-function displayApllianceOptions() {
-  const list = document.querySelector(".section-filters__custom--select");
-  console.log(list);
-  // boucle for dans le tableau des appareils de la fonction getAllAppliancesFromRecipes
+function displayApllianceOptions(options) {
+  const list = document.querySelector("#green");
+  list.innerHTML += options.map((option) => `<p>${option}</p>.join`);
 }
-displayOptions();
