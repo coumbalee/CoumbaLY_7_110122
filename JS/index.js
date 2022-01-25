a6833bc3d285a1839233c47d059d647faca8c844
coumbalee <coumba.ly@yahoo.com>
2022-01-25 16:18:01 +0100

    Suppression generateHeader et generateSearchBar

--- JS/index.js
+++ JS/index.js
@@ -1,16 +1,6 @@
 "use strict";
-import { recipes } from "./recipes.js";
-import {
-  getAllAppliancesFromRecipes,
-  getAllUstensilsFromRecipes,
-  getAllIngredientsFromRecipes,
-} from "./dataRecipes.js";
 import { displayDropdowns } from "./dropdowns.js";
-import { generateSearchBar } from "./searchBar.js";
-
 async function displayPage() {
-  // generateHeader();
-  generateSearchBar();
   displayDropdowns();
 }
 displayPage();
