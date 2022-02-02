e0da4a965fc8729d3228dc41c2cee79cd46bad4a
coumbalee <coumba.ly@yahoo.com>
2022-02-02 22:03:47 +0100

    displayCards

--- JS/cards.js
+++ JS/cards.js
@@ -1,1 +1,31 @@
 import Card from "./CardClass.js";
+console.log(Card);
+import { recipes } from "./recipes.js";
+
+//  générer une card pour chaque recette
+export function displayCards(
+  name,
+  time,
+  ingredients,
+  description,
+  appliance,
+  ustensils
+) {
+  let cards = [];
+  const sectionCards = document.querySelector(".section-cards");
+  for (const recipe of recipes) {
+    console.log(recipe);
+    cards.push(
+      new Card(
+        recipe.name,
+        recipe.time,
+        recipe.ingredients,
+        recipe.description,
+        recipe.appliance,
+        recipe.ustensils
+      )
+    );
+    console.log(cards);
+  }
+  sectionCards.innerHTML = `${cards}`;
+}
