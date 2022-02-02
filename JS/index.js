e5f15d0fcb2e5fe6718e8d4bc42ae8b1ba301e01
coumbalee <coumba.ly@yahoo.com>
2022-02-02 22:04:38 +0100

    displayCards

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,12 @@
 "use strict";
 import { displayDropdowns } from "./dropdowns.js";
+import { displayCards } from "./cards.js";
+
+// import Tag from "./TagsClass.js";
+// console.log(Tag);
 
 async function displayPage() {
   displayDropdowns();
+  displayCards();
 }
 displayPage();
