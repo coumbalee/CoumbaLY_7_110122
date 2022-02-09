adfddc879e9fdcc6da14c49794d11b700ab16137
coumbalee <coumba.ly@yahoo.com>
2022-02-09 21:18:40 +0100

    displayCards ()

--- JS/cards.js
+++ JS/cards.js
@@ -2,7 +2,6 @@ import Card from "./CardClass.js";
 console.log(Card);
 import { recipes } from "./recipes.js";
 
-//  générer une card pour chaque recette
 export function displayCards(
   name,
   time,
@@ -12,20 +11,41 @@ export function displayCards(
   ustensils
 ) {
   let cards = [];
-  const sectionCards = document.querySelector(".section-cards");
+  const list = document.querySelector(".section-cards__list");
   for (const recipe of recipes) {
-    console.log(recipe);
-    cards.push(
-      new Card(
-        recipe.name,
-        recipe.time,
-        recipe.ingredients,
-        recipe.description,
-        recipe.appliance,
-        recipe.ustensils
-      )
+    cards = new Card(
+      recipe.name,
+      recipe.time,
+      recipe.ingredients,
+      recipe.description,
+      recipe.appliance,
+      recipe.ustensils
     );
     console.log(cards);
   }
-  sectionCards.innerHTML = `${cards}`;
+  list.innerHTML = recipes
+    .map(
+      (recipe) => `<li class="card">
+      <a href="#">
+          <div class="card__thumb"></div>
+          <div class="card__body">
+              <div class="card__head">
+                  <h2 class="card__title">${recipe.name}</h2>
+                  <div class="card__time">
+                      <i class="far fa-clock"></i>
+                      <p class="card__minutes">${recipe.time} min</p>
+                  </div>
+              </div>
+              <div class="card__content">
+                   <ul class="card__ingredients">${recipe.ingredients}
+                  </ul>
+                  <p class="card__description">${recipe.description}</p>
+              </div>
+          </div>
+      </a>
+  </li>`
+    )
+    .join("");
+}
+{
 }
